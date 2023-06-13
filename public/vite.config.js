a2f7575e15a113132d075c5391dd8426747ec990
ijeoma <ijeomaodinko@gmail.com>
2023-06-13 22:57:00 +0100

    Add files

--- public/vite.config.js
+++ public/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxInject: "import React from 'react'",
+  },
+  optimizeDeps: {
+    include: ['@swc/core'],
+  },
+  optimizeDeps: {
+    exclude: ['@swc/wasm'],
+  },
 })
