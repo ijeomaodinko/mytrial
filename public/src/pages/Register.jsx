63c5fb39e22024f2c614b4fd21f636a9469932cf
ijeoma <ijeomaodinko@gmail.com>
2023-06-19 11:51:13 +0100

    Add register files

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import Logo from '../assets/logo.png';
+// import Logo from '../assets/logo.png';
 
 const Register = () => {
 
@@ -15,7 +15,7 @@ const Register = () => {
       <FormContainer>
         <form onSubmit={(event) => handleSubmit(event)}>
             <div className='brand'>
-                <img src="Logo"  alt="Logo" />
+                <img src=""  alt="Logo" />
                 <h1>MiChat</h1>
             </div>
             <input 
@@ -45,4 +45,63 @@ display: flex;
 flex-direction: column;
 justify-content: center;
 gap: 1;
+align-items: center;
+background-color: #131324;
+.brand {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  justify-content: center;
+}
+  img {
+    height: 5rem;
+  }
+  h1 {
+    color: white;
+    text-transform: uppercase;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #0000070;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4ef;
+      color: #fff;
+      width: 100%;
+      font-size: 1rem;
+      &:focus {
+        border: 0.1rem solid #1ee;
+        outline: none;
+      }
+    }
+    button {
+      background-color: #997af;
+      color: #fff;
+      padding: 1rem 2rem;
+      border: none;
+      font-weigth: bold;
+      cursor: pointer;
+      border-radius: 0.4rem;
+      font-size: 1rem;
+      text-transform: uppercase;
+      transition: 0.5s ease-in-out;
+      &:hover {
+        background-color: #4e0eff;
+      }
+    }
+  span {
+    color: white;
+    text-transfrom: uppercase;
+    a {
+      color: #4e0eff;
+      text-decoration: none;
+      font-weigth: bold;
+    }
+  }
+  }
 `;
